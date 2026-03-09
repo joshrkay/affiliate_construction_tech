6dd0ec17c14f01621f1c84e64a49f5872fbd6229
Josh <60823931+joshrkay@users.noreply.github.com>
2026-03-09 10:32:39 -0700

    Revert "Integrate Figma source implementation"

--- app/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { RouterProvider } from "react-router";
-import { router } from "./routes";
-export default function App() {
-    return _jsx(RouterProvider, { router: router });
-}
